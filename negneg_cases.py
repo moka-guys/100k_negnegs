199afec849556e8a8e3e4f0ed488e7bb9e0c85d6
Andy Bond <andybond87@gmail.com>
2019-02-05 11:33:53 +0000

    Take account of multiple cip versions

--- negneg_cases.py
+++ negneg_cases.py
@@ -39,20 +39,28 @@ def group_vars_by_cip(interpreted_genomes_json):
     Args:
         interpreted_genomes_json: interpreted genome JSON from CIP API
     Returns:
-        Dictionary of variants grouped by CIP (key = CIP, value = list of variant objects)
+        Nested dictionary of variants grouped by CIP and cip version. {key = CIP value = {key = cip_version, value = list_of_variants}}
+
     """
     # Note this does not pull out CNVs/SVs
     vars_by_cip = {}
-    # possible values for cip at time of writing: 
+    # possible values for cip at time of writing:
     # omicia, congenica, nextcode, genomics_england, illumina, exomiser
     # There will be a separate interepreted genome for each cip used
+
     for ig in interpreted_genomes_json:
         # Convert the interpreted genome JSON into InterpretedGenome object from GeL Report Models v6.0
         ig_obj = InterpretedGenome.fromJsonDict(ig['interpreted_genome_data'])
         # cip provider stored in the interpretationService field.
         # Store the list of reported variants for that cip
         if ig_obj.variants:
-            vars_by_cip[ig_obj.interpretationService.lower()] = ig_obj.variants
+            cip = ig_obj.interpretationService.lower()
+            cip_version = int(ig['cip_version'])
+            # If cip not already in dictionary, add it in with an empty dictionary as value
+            vars_by_cip[cip] = vars_by_cip.setdefault(cip, {})
+            # Add the variants for that cip/version to dicitionary.
+            # If the same cip/version is already there, just append to the existing list.
+            vars_by_cip[cip][cip_version] = vars_by_cip[cip].setdefault(cip_version, []) + ig_obj.variants
     return vars_by_cip
 
 def group_vars_by_tier(variants_json):
@@ -61,7 +69,7 @@ def group_vars_by_tier(variants_json):
     Args:
         variants_json: list of GeL report model v6 variant objects
     Returns:
-        Dictionary of variants grouped by tier (key = tier, value = list of variant objects)
+        Dictionary of variants grouped by tier {key = tier, value = list of variant objects}
     """
     # Takes list of variants and groups them by tier (Note this doesn't include SVs/CNVs)
     tiered_vars = {
@@ -80,15 +88,15 @@ def group_vars_by_tier(variants_json):
                 tiers.append(int(reportevent.tier.strip('TIER')))
             else:
                 # Value of 4 used to represent any other or no tier.
-                tiers.append(4)                    
+                tiers.append(4)
         # The lowest number represents the highest ranked tier for that variant (e.g. tier 1 ranks higher than tier 2)
         top_rank_tier = min(tiers)
         # Record the variant in the dictionary based on it's highest ranked tier
         if top_rank_tier == 4:
             tiered_vars['OTHER'].append(variant)
         else:
-            tiered_vars['TIER{tier}'.format(tier=top_rank_tier)].append(variant)
-    return tiered_vars    
+            tiered_vars[f'TIER{top_rank_tier}'].append(variant)
+    return tiered_vars
 
 def is_neg_neg(ir_json, cips=[]):
     """
@@ -101,8 +109,10 @@ def is_neg_neg(ir_json, cips=[]):
     """
     # Create a dictionary of variant lists (value) grouped by CIP (key - lowercase)
     vars_by_cip = group_vars_by_cip(ir_json['interpreted_genome'])
+    # There may be multiple interpreted genomes for a single CIP, so take the one with the highest cip_version number
+    max_version = max(vars_by_cip['genomics_england_tiering'].keys())
     # Group variants from genomics_england_tiering by tier
-    gel_tiered_vars = group_vars_by_tier(vars_by_cip['genomics_england_tiering'])
+    gel_tiered_vars = group_vars_by_tier(vars_by_cip['genomics_england_tiering'][max_version])
     # negneg if no non-tier3 or cip candidate variants
     num_tier1 = len(gel_tiered_vars['TIER1'])
     num_tier2 = len(gel_tiered_vars['TIER2'])
@@ -110,7 +120,10 @@ def is_neg_neg(ir_json, cips=[]):
     # Initialise cip_candidates variable to zero, then loop through cips counting variants
     cip_candidates = 0
     for cip in cips:
-        cip_candidates += len(vars_by_cip[cip]) if (cip in vars_by_cip) else 0
+        if cip in vars_by_cip:
+            # There may be multiple interpreted genomes for a single CIP, so take the one with the highest cip_version number
+            max_version = max(vars_by_cip[cip].keys())
+            cip_candidates += len(vars_by_cip[cip][max_version])
     # Return true if it's a negative negative, otherwise return false.
     if sum((num_tier1, num_tier2, num_other, cip_candidates)) == 0:
         return True
@@ -120,7 +133,7 @@ def group_cases():
     """
     Groups all Guys cases in CIP-API based on whether they are negative negatives
     Returns:
-        Dictionary of grouped cases (key = group, value = list of case JSONs from CIP-API) 
+        Dictionary of grouped cases {key = group, value = list of case JSONs from CIP-API}
     """
     # Pull out all cases that are either ready for interpretation or have been reported
     sent_to_gmcs = get_interpretation_request_list(last_status='sent_to_gmcs', sample_type='raredisease')
@@ -130,11 +143,12 @@ def group_cases():
     num_requests = Counter([case['proband'] for case in sent_to_gmcs + report_generated + report_sent])
     # Filter cases that are awaiting interpretation to only include Guys cases
     guys_cases = (case for case in sent_to_gmcs if ('RJ1' in case['sites'] or 'RJ101' in case['sites'] or 'GSTT' in case['sites']))
-    # Cases will be grouped as below. 
+    # Cases will be grouped as below.
     # 'negnegs_one_request' = negneg cases where there are no other ongoing or reported interpretation requests for that patient - can be reported automatically
-    # 'negnegs_multiple_requests' = negneg cases where there are other active or reported interpretation requests for that patient (which may not be negneg) 
+    # 'negnegs_multiple_requests' = negneg cases where there are other active or reported interpretation requests for that patient (which may not be negneg)
     # 'error' = Error encountered when trying to parse the CIP-API data for this case. Some early pilot cases have broken formatting so they end up here.
     # 'all_other' = Everything else.
+
     grouped_cases = {
         'negnegs_one_request': [],
         'negnegs_multiple_requests': [],
@@ -142,34 +156,34 @@ def group_cases():
         'all_other': []
     }
     for case in guys_cases:
-            # Capture interpretation request ID and version
-            ir_id = case['interpretation_request_id'].split('-')[0]
-            ir_version = case['interpretation_request_id'].split('-')[1]
-            participant_id = case['proband']
-            # Return JSON from CIP API, use report models v6
-            ir_json = get_interpretation_request_json(ir_id, ir_version, reports_v6=True)
-            # Check if case is a negneg
-            # Some very old pulot cases have broken formatting causing error here, so catch these with try/except
-            try:
-                negneg = is_neg_neg(ir_json, cips=['omicia', 'congenica'])            
-            except:
-                grouped_cases['error'].append(case)
-                continue
-            # If it is negneg, check if there's any other active or reported requests for the same participant 
-            if negneg and num_requests[participant_id] == 1:
-                grouped_cases['negnegs_one_request'].append(case)
-            elif negneg:
-                grouped_cases['negnegs_multiple_requests'].append(case)
-            # Else if it's not negneg
-            else:
-                grouped_cases['all_other'].append(case)
+        # Capture interpretation request ID and version
+        ir_id = case['interpretation_request_id'].split('-')[0]
+        ir_version = case['interpretation_request_id'].split('-')[1]
+        participant_id = case['proband']
+        # Return JSON from CIP API, use report models v6
+        ir_json = get_interpretation_request_json(ir_id, ir_version, reports_v6=True)
+        # Check if case is a negneg
+        # Some very old pilot cases have broken formatting causing error here, so catch these with try/except
+        try:
+            negneg = is_neg_neg(ir_json, cips=['omicia', 'congenica'])
+        except:
+            grouped_cases['error'].append(case)
+            continue
+        # If it is negneg, check if there's any other active or reported requests for the same participant
+        if negneg and num_requests[participant_id] == 1:
+            grouped_cases['negnegs_one_request'].append(case)
+        elif negneg:
+            grouped_cases['negnegs_multiple_requests'].append(case)
+        # Else if it's not negneg
+        else:
+            grouped_cases['all_other'].append(case)
     return grouped_cases
 
 def main():
     # Get command line arguments
     args = process_arguments()
     out_file = args.output_file
-    # Open output file and write headers    
+    # Open output file and write headers
     with open(out_file, 'w') as output_file:
         output_file.write('participant_ID\tCIP_ID\tgroup\n')
         # Group cases according to variants found in CIP-API
